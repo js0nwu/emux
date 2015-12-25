d0cb26b24215881dda0b9e56183a4c4efad831ee
Jason Wu <jasonwupilly1997@gmail.com>
2015-12-24 19:58:04 -0500

    use librosa for mfcc

--- syncer.py
+++ syncer.py
@@ -15,6 +15,7 @@ import blender
 import queue
 
 import librosa.effects as re
+import librosa.features as rf
 
 import numpy.linalg
 
@@ -22,60 +23,11 @@ import os
 
 PATH_STEPS = [1/2, 2/3, 3/4, 1, 4/3, 3/2, 2/1]
 
-FRAME_LENGTH = 0.5
-
-
-def mel(hertz):
-    return 1125 * numpy.log(1 + hertz / 700)
-
-
-def mel_inv(mels):
-    return 700 * (numpy.exp(mels / 1125) - 1)
-
-
-def filterbanks(low, high, num):
-    return [mel_inv(x) for x in list(range(int(mel(low)), int(mel(high)), int((mel(high) - mel(low)) / (num - 1))))]
-
-
-def hf(f, m, k):
-    if k < f[max(0, m - 1)]:
-        return 0
-    elif k >= f[m - 1] and k < f[m]:
-        return (k - f[m - 1]) / (f[m] - f[m - 1])
-    elif k >= f[m] and k < f[m + 1]:
-        return (f[m + 1] - k) / (f[m + 1] - f[m])
-    elif k >= f[min(len(f) - 1, m + 1)]:
-        return 0
-    else:
-        return 0
-
-
-def frame_power_transform(frame):
-    n = 256
-    window = hamming(frame.size)
-    frame_dft = numpy.multiply(fft(window * frame, n, axis=0), (1 / numpy.sqrt(n)))
-    frame_power = numpy.abs(numpy.square(frame_dft))
-    return frame_power
-
-
-def signal_energy(frame):
-    return numpy.sum(frame_power_transform(frame))
-
-
-def mfcc(frame, frequency):
-    frame_power = frame_power_transform(frame)
-    # code to auto determine filter bank frequency range based on input audio sample rate
-    frame_banks = [int(((frame_power.size + 1) * x) / frequency) for x in filterbanks(200, int(frequency / 2), 12)]
-    max_bank = numpy.max(numpy.asarray(frame_banks))
-    frame_mels = list(itertools.starmap(lambda x, y: hf(frame_banks, x, y) * frame_power[y],
-                                        itertools.product(range(len(frame_banks)), range(max_bank))))
-    frame_ampl = [numpy.log(numpy.sum(x)) if x != 0 else 0 for x in frame_mels]
-    return dct(frame_ampl, axis=0)
-
+FRAME_LENGTH = 0.1
 
 def mfcc_distance(r1, s1, r2, s2):
-    a = mfcc(s1, r1)
-    b = mfcc(s2, r2)
+    a = rf.mfcc(s1, r1)
+    b = rf.mfcc(s2, r2)
     return numpy.linalg.norm(b - a)
 
 def extract_frame(r, s, t, l=FRAME_LENGTH):
