a176585fe303c98d64a5162269c1135e97bcbfc1
Jason Wu <jasonwupilly1997@gmail.com>
2015-12-25 23:39:10 -0500

    TIL lists are thread safe in python

--- emopillow.py
+++ emopillow.py
@@ -11,18 +11,10 @@ from threading import Thread
 VIDEO_A = "angry.mp4"
 VIDEO_B = "sad.mp4"
 
-OUTPUT_FPS = 10
+OUTPUT_FPS = 0.25
 
 a = VideoFileClip(VIDEO_A)
 b = VideoFileClip(VIDEO_B)
-
-# a_frame = cv2.cvtColor(a.get_frame(6.6), cv2.COLOR_RGB2BGR)
-# b_frame = cv2.cvtColor(b.get_frame(6.6), cv2.COLOR_RGB2BGR)
-#
-# c_frame = blender.generate_midframe(a_frame, b_frame, 0.95)
-#
-# cv2.imwrite("c_frame.jpg", c_frame)
-
 
 synced_b = syncer.sync_clips(a, b)
 
@@ -48,22 +40,24 @@ def mix_audio_frame(a, b, factor):
     return (1.0 - factor) * a + factor * b
 
 
-def process_frame(a, b, factor, framesequence):
+def process_frame(a, b, factor):
     frame_a = cv2.cvtColor(a, cv2.COLOR_RGB2BGR)
     frame_b = cv2.cvtColor(b, cv2.COLOR_RGB2BGR)
     frame_c = blender.generate_midframe(frame_a, frame_b, factor)
-    framesequence.append(frame_c)
+    frames.append(frame_c)
 
 while time < a.duration:
     print("frame at " + str(time))
-    t = Thread(target=process_frame, args=(a.get_frame(time), synced_b.get_frame(time), get_factor(time), frames))
-    t.start()
+    process_frame(a.get_frame(time), synced_b.get_frame(time), get_factor(time))
     time += tstep
 
+print(frames)
 images = ImageSequenceClip(frames, fps=OUTPUT_FPS)
 a_audio = a.audio.to_soundarray()
+print(a_audio)
+print(a_audio.shape)
 a_audio = blender.stereo_to_mono(a_audio)
-a = a.set_audio(AudioArrayClip(a_audio, fps=a.audio.fps))
+a = a.set_audio(AudioArrayClip([a_audio], a.audio.fps))
 make_frame = lambda t: mix_audio_frame(a.audio.get_frame(t), synced_b.audio.get_frame(t), get_factor(t))
 audios = AudioClip(make_frame, duration=a.audio.duration)
 images = images.set_audio(audios)
