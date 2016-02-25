f515bf309279f2e6e8f3eded1276535541927030
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-24 21:36:45 -0500

    remove original picture calculation in transformation

--- lolswap.py
+++ lolswap.py
@@ -12,7 +12,7 @@ num = int(sys.argv[3])
 a = cv2.imread(picture_path)
 b = cv2.imread(replace_path)
 
-for i in range(num + 1):
+for i in range(1, num + 1):
     c = blender.generate_midframe(a, b, float(i) / float(num))
     cv2.imwrite(OUTPUT_PREFIX + str(i) + OUTPUT_EXTENSION, c)
     print("processed " + str(i + 1))
